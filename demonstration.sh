7de9bce00ccc66b12b2d0e85320186243b1a9cab
Neo Tlaletsi <ntlaletsi86@gmail.com>
2025-01-05 11:59:38 +0200

    Demo # Screenshots, mock creds terra provider

--- demonstration.sh
+++ demonstration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-kind create cluster -n example --config kind_config.yaml
+kind create cluster -n example --config kind-cluster/kind_config.yaml
 kubectl cluster-info --context kind-example
 
 helm repo add crossplane-stable https://charts.crossplane.io/stable
@@ -8,6 +8,11 @@ helm repo add localstack-charts https://localstack.github.io/helm-charts
 helm install crossplane crossplane-stable/crossplane --namespace crossplane-system --create-namespace
 helm install localstack localstack-charts/localstack --namespace localstack --create-namespace
 helm repo update
+# wait
+
+
+kubectl create secret generic aws-secret -n crossplane-system --from-file=creds=secrets/./aws-credentials.txt
+kubectl apply -f secrets/localstack-aws-credentials.yaml -n crossplane-system
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
@@ -17,7 +22,7 @@ export NODE_PORT=$(kubectl get --namespace "localstack" -o jsonpath="{.spec.port
 export NODE_IP=$(kubectl get nodes --namespace "localstack" -o jsonpath="{.items[0].status.addresses[0].address}")
 
 source $HOME/.bashrc
-source $HOME/.bach_profile
+source $HOME/.bash_profile
 
 
 kubectl create secret generic aws-secret -n crossplane-system --from-file=creds=secrets/./aws-credentials.txt
